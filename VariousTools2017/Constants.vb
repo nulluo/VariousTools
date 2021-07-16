49b95121c93d18d2fe67777f2fb854def6c45b0e
nulluo <nulluo.pg@gmail.com>
2021-07-16 09:26:34 +0900

    VS2019->VS2017

--- VariousTools2017/Constants.vb
+++ VariousTools2017/Constants.vb
@@ -3,7 +3,7 @@
     ''' <summary>
     ''' 制限時間(単位:時間)
     ''' </summary>
-    Public Const Limit As Integer = 10
+    Public Const Limit As Integer = 9
 
     Public Const ProcessName As String = "10HourTimer"
 
--- VariousTools2017/Form1.Designer.vb
+++ VariousTools2017/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.startButton = New System.Windows.Forms.Button()
@@ -39,9 +39,10 @@ Partial Class Form1
         '
         'startButton
         '
-        Me.startButton.Location = New System.Drawing.Point(56, 363)
+        Me.startButton.Location = New System.Drawing.Point(74, 342)
+        Me.startButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.startButton.Name = "startButton"
-        Me.startButton.Size = New System.Drawing.Size(158, 68)
+        Me.startButton.Size = New System.Drawing.Size(149, 50)
         Me.startButton.TabIndex = 0
         Me.startButton.Text = "Start"
         Me.startButton.UseVisualStyleBackColor = True
@@ -49,18 +50,18 @@ Partial Class Form1
         'nowTimeLabel
         '
         Me.nowTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.nowTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.nowTimeLabel.Location = New System.Drawing.Point(56, 58)
+        Me.nowTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.nowTimeLabel.Location = New System.Drawing.Point(53, 42)
         Me.nowTimeLabel.Name = "nowTimeLabel"
-        Me.nowTimeLabel.Size = New System.Drawing.Size(351, 67)
+        Me.nowTimeLabel.Size = New System.Drawing.Size(381, 87)
         Me.nowTimeLabel.TabIndex = 1
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(13, 13)
+        Me.Label1.Location = New System.Drawing.Point(12, 10)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(117, 41)
+        Me.Label1.Size = New System.Drawing.Size(109, 30)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Current"
         '
@@ -72,45 +73,46 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(506, 17)
+        Me.Label2.Location = New System.Drawing.Point(476, 12)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(79, 41)
+        Me.Label2.Size = New System.Drawing.Size(77, 30)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Start"
         '
         'startTimeLabel
         '
         Me.startTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.startTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.startTimeLabel.Location = New System.Drawing.Point(517, 58)
+        Me.startTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.startTimeLabel.Location = New System.Drawing.Point(487, 42)
         Me.startTimeLabel.Name = "startTimeLabel"
-        Me.startTimeLabel.Size = New System.Drawing.Size(168, 67)
+        Me.startTimeLabel.Size = New System.Drawing.Size(209, 87)
         Me.startTimeLabel.TabIndex = 1
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(506, 170)
+        Me.Label4.Location = New System.Drawing.Point(476, 124)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(68, 41)
+        Me.Label4.Size = New System.Drawing.Size(60, 30)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "End"
         '
         'endTimeLabel
         '
         Me.endTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.endTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.endTimeLabel.Location = New System.Drawing.Point(517, 211)
+        Me.endTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.endTimeLabel.Location = New System.Drawing.Point(487, 154)
         Me.endTimeLabel.Name = "endTimeLabel"
-        Me.endTimeLabel.Size = New System.Drawing.Size(168, 67)
+        Me.endTimeLabel.Size = New System.Drawing.Size(209, 87)
         Me.endTimeLabel.TabIndex = 1
         '
         'resetButton
         '
         Me.resetButton.Enabled = False
-        Me.resetButton.Location = New System.Drawing.Point(622, 363)
+        Me.resetButton.Location = New System.Drawing.Point(606, 342)
+        Me.resetButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.resetButton.Name = "resetButton"
-        Me.resetButton.Size = New System.Drawing.Size(158, 68)
+        Me.resetButton.Size = New System.Drawing.Size(149, 50)
         Me.resetButton.TabIndex = 0
         Me.resetButton.Text = "Reset"
         Me.resetButton.UseVisualStyleBackColor = True
@@ -118,36 +120,37 @@ Partial Class Form1
         'countDownLabel
         '
         Me.countDownLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.countDownLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
-        Me.countDownLabel.Location = New System.Drawing.Point(56, 238)
+        Me.countDownLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.countDownLabel.Location = New System.Drawing.Point(53, 174)
         Me.countDownLabel.Name = "countDownLabel"
-        Me.countDownLabel.Size = New System.Drawing.Size(351, 67)
+        Me.countDownLabel.Size = New System.Drawing.Size(381, 87)
         Me.countDownLabel.TabIndex = 1
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(13, 193)
+        Me.Label5.Location = New System.Drawing.Point(12, 141)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(185, 41)
+        Me.Label5.Size = New System.Drawing.Size(166, 30)
         Me.Label5.TabIndex = 2
         Me.Label5.Text = "Count Down"
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(330, 363)
+        Me.Button1.Location = New System.Drawing.Point(332, 342)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(188, 58)
+        Me.Button1.Size = New System.Drawing.Size(177, 42)
         Me.Button1.TabIndex = 3
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(17.0!, 41.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 30.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(813, 461)
+        Me.ClientSize = New System.Drawing.Size(893, 453)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
@@ -160,6 +163,7 @@ Partial Class Form1
         Me.Controls.Add(Me.resetButton)
         Me.Controls.Add(Me.startButton)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
