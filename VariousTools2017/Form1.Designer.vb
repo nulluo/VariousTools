f9eac6a2c3e2ed514162d9f4b66482eca98679d3
nulluo <nulluo.pg@gmail.com>
2021-07-26 23:40:14 +0900

    - 10時間オーバしたら22時(深夜残業)が次のデッドラインなので、それも通知するように機能追加 - アプリケーション設定ファイルに開始時刻を書き出して、ちゃんとアプリを落として同日に再起動した場合もタイマーが有効になるよう改善

--- VariousTools2017/Form1.Designer.vb
+++ VariousTools2017/Form1.Designer.vb
@@ -35,11 +35,16 @@ Partial Class Form1
         Me.countDownLabel = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.countdownFor22Label = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.resetEndTimeButton = New System.Windows.Forms.Button()
+        Me.deadEndTimeLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'startButton
         '
-        Me.startButton.Location = New System.Drawing.Point(74, 342)
+        Me.startButton.Location = New System.Drawing.Point(76, 474)
         Me.startButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.startButton.Name = "startButton"
         Me.startButton.Size = New System.Drawing.Size(149, 50)
@@ -73,7 +78,7 @@ Partial Class Form1
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(476, 12)
+        Me.Label2.Location = New System.Drawing.Point(1014, 10)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(77, 30)
         Me.Label2.TabIndex = 2
@@ -83,7 +88,7 @@ Partial Class Form1
         '
         Me.startTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.startTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
-        Me.startTimeLabel.Location = New System.Drawing.Point(487, 42)
+        Me.startTimeLabel.Location = New System.Drawing.Point(1025, 40)
         Me.startTimeLabel.Name = "startTimeLabel"
         Me.startTimeLabel.Size = New System.Drawing.Size(209, 87)
         Me.startTimeLabel.TabIndex = 1
@@ -91,7 +96,7 @@ Partial Class Form1
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(476, 124)
+        Me.Label4.Location = New System.Drawing.Point(1014, 145)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(60, 30)
         Me.Label4.TabIndex = 2
@@ -101,7 +106,7 @@ Partial Class Form1
         '
         Me.endTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.endTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
-        Me.endTimeLabel.Location = New System.Drawing.Point(487, 154)
+        Me.endTimeLabel.Location = New System.Drawing.Point(1025, 175)
         Me.endTimeLabel.Name = "endTimeLabel"
         Me.endTimeLabel.Size = New System.Drawing.Size(209, 87)
         Me.endTimeLabel.TabIndex = 1
@@ -109,7 +114,7 @@ Partial Class Form1
         'resetButton
         '
         Me.resetButton.Enabled = False
-        Me.resetButton.Location = New System.Drawing.Point(606, 342)
+        Me.resetButton.Location = New System.Drawing.Point(608, 461)
         Me.resetButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.resetButton.Name = "resetButton"
         Me.resetButton.Size = New System.Drawing.Size(149, 50)
@@ -121,7 +126,7 @@ Partial Class Form1
         '
         Me.countDownLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.countDownLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
-        Me.countDownLabel.Location = New System.Drawing.Point(53, 174)
+        Me.countDownLabel.Location = New System.Drawing.Point(55, 293)
         Me.countDownLabel.Name = "countDownLabel"
         Me.countDownLabel.Size = New System.Drawing.Size(381, 87)
         Me.countDownLabel.TabIndex = 1
@@ -129,7 +134,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(12, 141)
+        Me.Label5.Location = New System.Drawing.Point(14, 260)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(166, 30)
         Me.Label5.TabIndex = 2
@@ -137,7 +142,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(332, 342)
+        Me.Button1.Location = New System.Drawing.Point(334, 461)
         Me.Button1.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(177, 42)
@@ -145,19 +150,70 @@ Partial Class Form1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'countdownFor22Label
+        '
+        Me.countdownFor22Label.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.countdownFor22Label.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.countdownFor22Label.Location = New System.Drawing.Point(505, 293)
+        Me.countdownFor22Label.Name = "countdownFor22Label"
+        Me.countdownFor22Label.Size = New System.Drawing.Size(381, 87)
+        Me.countdownFor22Label.TabIndex = 1
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(464, 260)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(284, 30)
+        Me.Label6.TabIndex = 2
+        Me.Label6.Text = "Count Down for 22:00"
+        '
+        'resetEndTimeButton
+        '
+        Me.resetEndTimeButton.Location = New System.Drawing.Point(852, 461)
+        Me.resetEndTimeButton.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.resetEndTimeButton.Name = "resetEndTimeButton"
+        Me.resetEndTimeButton.Size = New System.Drawing.Size(371, 50)
+        Me.resetEndTimeButton.TabIndex = 4
+        Me.resetEndTimeButton.Text = "ResetEndTime"
+        Me.resetEndTimeButton.UseVisualStyleBackColor = True
+        '
+        'deadEndTimeLabel
+        '
+        Me.deadEndTimeLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.deadEndTimeLabel.Font = New System.Drawing.Font("Yu Gothic UI", 14.25!)
+        Me.deadEndTimeLabel.Location = New System.Drawing.Point(1019, 334)
+        Me.deadEndTimeLabel.Name = "deadEndTimeLabel"
+        Me.deadEndTimeLabel.Size = New System.Drawing.Size(209, 87)
+        Me.deadEndTimeLabel.TabIndex = 1
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(1008, 304)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(132, 30)
+        Me.Label7.TabIndex = 2
+        Me.Label7.Text = "Dead End"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(16.0!, 30.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
-        Me.ClientSize = New System.Drawing.Size(893, 453)
+        Me.ClientSize = New System.Drawing.Size(1267, 555)
+        Me.Controls.Add(Me.resetEndTimeButton)
         Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.deadEndTimeLabel)
         Me.Controls.Add(Me.endTimeLabel)
         Me.Controls.Add(Me.startTimeLabel)
+        Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.countdownFor22Label)
         Me.Controls.Add(Me.countDownLabel)
         Me.Controls.Add(Me.nowTimeLabel)
         Me.Controls.Add(Me.resetButton)
@@ -165,7 +221,7 @@ Partial Class Form1
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
         Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "10HourTimer"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -183,4 +239,9 @@ Partial Class Form1
     Friend WithEvents countDownLabel As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Button1 As Button
+    Friend WithEvents countdownFor22Label As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents resetEndTimeButton As Button
+    Friend WithEvents deadEndTimeLabel As Label
+    Friend WithEvents Label7 As Label
 End Class
--- VariousTools2017/Form1.vb
+++ VariousTools2017/Form1.vb
@@ -1,53 +1,158 @@
 ﻿Public Class Form1
 
-    Private Property Duration As Integer
-    Private Property EndTime As DateTime
 
-    Private Property IsStarted As Boolean
+#Region "Property"
+    'UNIMPLEMENTED: 何のために用意したプロパティだっけ？これ
+    'Private Property Duration As Integer
+
+    Dim _endTime As DateTime
+    ''' <summary>
+    ''' カウントダウン終了時刻
+    ''' </summary>
+    ''' <returns>カウントダウン終了時刻</returns>
+    Private Property EndTime As DateTime
+        Get
+            Return _endTime
+        End Get
+        Set(value As DateTime)
+            _endTime = value
+            My.Settings.EndTime = _endTime
+            My.Settings.Save()
+            Me.endTimeLabel.Text = _endTime.ToString("t")
+            Me.deadEndTimeLabel.Text = _endTime.AddHours(1).ToString("t")
+        End Set
+    End Property
+
+    Dim _startTime As DateTime
+    Private Property StartTime As DateTime
+        Get
+            Return _startTIme
+        End Get
+        Set(value As DateTime)
+            _startTime = value
+            Me.startTimeLabel.Text = _startTime.ToString("t")
+            My.Settings.StartTime = _startTime
+            My.Settings.Save()
+
+            'UNIMPLEMENTED: StartTimeプロパティに値がセットされたタイミングで自動的にEndTimeに値が入るような連動プロパティにしてOK
+            'UNIMPLEMENTED: ↑そうすると、EndTimeはApp.configに持つ必要がなくなる?←でもデバッグしやすくない？持ってたほうが
+        End Set
+    End Property
+
+    Dim _isCountdownOnGoing As Boolean
+    ''' <summary>
+    ''' カウントダウン継続中であるかどうか
+    ''' </summary>
+    ''' <returns>カウントダウン継続中であるかどうかを表す真理値</returns>
+    Private Property IsCountdownOnGoing As Boolean
+        Get
+            Return _isCountdownOnGoing
+        End Get
+        Set(onGoing As Boolean)
+            _isCountdownOnGoing = onGoing
+            If onGoing Then
+                Me.startButton.Enabled = False
+                Me.resetButton.Enabled = True
+            Else
+                'UNIMPLEMENTED: ここに,Falseが入った時の画面コントロールの表示制御を移動（ループに注意）
+
+            End If
+        End Set
+    End Property
+
+    ''' <summary>
+    ''' 22時を過ぎているかどうか
+    ''' </summary>
+    ''' <returns>22時を過ぎているかどうかを表す真理値</returns>
+    Private Property IsOver22 As Boolean
+
+    ''' <summary>
+    ''' 今日アラームが鳴動したかどうか
+    ''' </summary>
+    ''' <returns></returns>
+    Private Property IsAlarmed As Boolean
+
+#End Region
 
     'UNIMPLEMENTED: 2017に移行した時にソリューションとプロジェクトの単位が変わっちゃったのを修正する
-    'UNIMPLEMENTED: 10時間オーバしたら22時(深夜残業)が次のデッドラインなので、それも通知するように機能追加
-    'UNIMPLEMENTED: 端末を再起動した時→JSONなどの外部ファイルに開始時刻を書き出して、ちゃんと再起動後もタイマーが有効になるよう改善する
     'UNIMPLEMENTED: アラート音の鳴り方がおかしい（繰り返し鳴り続ける）のを改善
 
+    ''' <summary>
+    ''' 画面初期化
+    ''' </summary>
+    Private Sub InitializeForm()
+
+        Dim endTimeMySettings As Date = My.Settings.EndTime
+        Dim startTimeMySettings As Date = My.Settings.StartTime
+        If endTimeMySettings.CompareTo(Now) < 0 Then
+            '設定ファイルに保存されたカウントダウン終了時刻を過ぎている場合は、
+            '既に今日のカウントダウン終了時刻を過ぎた後か、
+            '設定ファイルの時刻は昨日以前に設定したカウントダウン終了時刻である場合なので
+            'カウントダウン実行中をOFFにする
+            Me.IsCountdownOnGoing = False
+
+            Me.startButton.Enabled = True
+            Me.resetButton.Enabled = False
+
+            Me.IsOver22 = False
+            Me.IsAlarmed = False
+
+            Me.startTimeLabel.Text = String.Empty
+            Me.endTimeLabel.Text = String.Empty
+            Me.countDownLabel.Text = String.Empty
+
+        Else
+            '設定ファイルに保存されたカウントダウン終了時刻を過ぎていない場合は、
+            'アラーム設定後に端末の再起動等でアプリが終了した場合なので、
+            '該当の終了時刻を再度セットする
+            Me.StartTime = startTimeMySettings
+            Me.EndTime = endTimeMySettings
+            Me.IsCountdownOnGoing = True
+        End If
+    End Sub
+
+#Region "Event Handler"
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.InitializeForm()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Me.nowTimeLabel.Text = Now.ToString("HH:mm:ss")
-        If IsStarted Then
-            Me.countDownLabel.Text = EndTime.Subtract(Now).ToString("hh\:mm\:ss")
 
+        '22時へのカウントダウン
+        If Not IsOver22 Then
+            Dim pm10oClock As Date
+            With Now()
+                pm10oClock = New Date(.Year, .Month, .Day, 22, 0, 0)
+            End With
+
+            If pm10oClock.CompareTo(Now) < 0 Then
+                IsOver22 = True
+                WindowFlasher.Flash()
+            Else
+                Dim countFor22 As TimeSpan
+                countFor22 = pm10oClock.Subtract(Now)
+                Me.countdownFor22Label.Text = countFor22.ToString("hh\:mm\:ss")
+            End If
+
+        End If
+
+        'EndTimeへのカウントダウン
+        If IsCountdownOnGoing Then
+            Me.countDownLabel.Text = EndTime.Subtract(Now).ToString("hh\:mm\:ss")
+            Debug.WriteLine("IsCountdownOnGoing")
             If EndTime.CompareTo(Now) < 0 Then
+                Me.IsAlarmed = True
+                Debug.WriteLine("EndTimeを過ぎた")
                 WindowFlasher.Flash()
             End If
         End If
     End Sub
 
     Private Sub startButton_Click(sender As Object, e As EventArgs) Handles startButton.Click
-        Me.startTimeLabel.Text = Now.ToString("t")
+        Me.StartTime = Now
         Me.EndTime = Now.AddHours(Constants.Limit)
-        Me.endTimeLabel.Text = EndTime.ToString("t")
-
-        Me.startButton.Enabled = False
-        Me.resetButton.Enabled = True
-        Me.IsStarted = True
-    End Sub
-
-    ''' <summary>
-    ''' 画面初期化
-    ''' </summary>
-    Private Sub InitializeForm()
-        Me.Duration = 0
-        Me.startButton.Enabled = True
-        Me.resetButton.Enabled = False
-
-        Me.IsStarted = False
-
-        Me.startTimeLabel.Text = String.Empty
-        Me.endTimeLabel.Text = String.Empty
-        Me.countDownLabel.Text = String.Empty
+        Me.IsCountdownOnGoing = True
     End Sub
 
     Private Sub resetButton_Click(sender As Object, e As EventArgs) Handles resetButton.Click
@@ -57,4 +162,10 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         WindowFlasher.Flash()
     End Sub
+
+    Private Sub resetEndTimeButton_Click(sender As Object, e As EventArgs) Handles resetEndTimeButton.Click
+        Me.EndTime = New Date(2000, 1, 1)
+    End Sub
+
+#End Region
 End Class
